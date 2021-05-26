ecd2629f0fb9dac8403038c728220777965faaf1
GGolfz <wisarutgolf11@gmail.com>
2021-05-26 22:48:13 +0700

    add

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,13 @@ Future<void> main() async {
   await _configureLocalTimeZone();
   const AndroidInitializationSettings initializationSettingsAndroid =
       AndroidInitializationSettings("@mipmap/ic_launcher");
-  const IOSInitializationSettings initializationSettingsIOS =
+  final IOSInitializationSettings initializationSettingsIOS =
       IOSInitializationSettings(
-          requestAlertPermission: true,
-          requestBadgePermission: true,
-          requestSoundPermission: true);
-  const InitializationSettings initializationSettings = InitializationSettings(
+    requestAlertPermission: true,
+    requestBadgePermission: true,
+    requestSoundPermission: true,
+  );
+  final InitializationSettings initializationSettings = InitializationSettings(
       android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
 
   await flutterLocalNotificationsPlugin.initialize(initializationSettings);
@@ -62,31 +63,59 @@ class _MyHomePageState extends State<MyHomePage> {
     const NotificationDetails platformChannelDatails =
         NotificationDetails(android: androidNotificationDetails);
 
-    await flutterLocalNotificationsPlugin.zonedSchedule(1, 'test notification',
-        'body', _nextInstanceOfTime(17, 34), platformChannelDatails,
-        uiLocalNotificationDateInterpretation:
-            UILocalNotificationDateInterpretation.absoluteTime,
-        androidAllowWhileIdle: true,
-        matchDateTimeComponents: DateTimeComponents.time);
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+      1,
+      'test notification',
+      'body',
+      _nextInstanceOfTime(0, 10, 14),
+      platformChannelDatails,
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.absoluteTime,
+      androidAllowWhileIdle: true,
+    );
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+      2,
+      'test notification',
+      'body',
+      _nextInstanceOfTime(1, 10, 14),
+      platformChannelDatails,
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.absoluteTime,
+      androidAllowWhileIdle: true,
+    );
   }
 
-  tz.TZDateTime _nextInstanceOfTime(int hour, int minute) {
+  Future<void> _getPendingNotification() async {
+    List<PendingNotificationRequest> temp =
+        await flutterLocalNotificationsPlugin.pendingNotificationRequests();
+    print(temp.length);
+  }
+
+  tz.TZDateTime _nextInstanceOfTime(int day, int hour, int minute) {
     final tz.TZDateTime now = tz.TZDateTime.now(tz.local);
-    tz.TZDateTime scheduledDate =
-        tz.TZDateTime(tz.local, now.year, now.month, now.day, hour, minute);
+    tz.TZDateTime scheduledDate = tz.TZDateTime(
+        tz.local, now.year, now.month, now.day + day, hour, minute);
     if (scheduledDate.isBefore(now)) {
-      scheduledDate = scheduledDate.add(const Duration(days: 1));
+      scheduledDate = scheduledDate.add(Duration(days: 1 + day));
     }
+    print(scheduledDate);
     return scheduledDate;
   }
 
   @override
   Widget build(BuildContext context) {
+    _showNotification();
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(),
+      body: Center(
+          child: TextButton(
+        child: Text("test"),
+        onPressed: () {
+          _getPendingNotification();
+        },
+      )),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           _showNotification();
